6473c32ebee6f07b0e2f012c33b6123333759c68
Juan G. Vargas Jr <jgvargasjr@outlook.com>
2022-09-15 22:18:28 -0500

    Some fixing fixes

--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
 let cal = document.getElementById('cal');
+let monthDetails = [];
+
+function setMonth() {
+    let currentDate = new Date();
+    monthDetails.push(currentDate.getDate())
+    console.log( monthDetails)
+}
 
 function createCal(currentMonth, totalDays, startDay) {
-    
+    let dayCounter = 1;    
     for (let i = 0; i < 5; i++) {
         let week = document.createElement('div')
         week.classList = "week";
@@ -9,12 +16,13 @@ function createCal(currentMonth, totalDays, startDay) {
         for (let j = 0; j < 7; j++) {
             let day = document.createElement('div')
             day.classList = "day";
-            day.textContent = `${j}`
+            day.textContent = `${dayCounter}`
             week.append(day)
+            dayCounter++;
         }
         cal.append(week)
     }  
-    console.log(cal)
 }
 
+setMonth()
 createCal()
